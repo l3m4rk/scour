190056036e0c6d460058aa37b7e49626453dc6bf
L3m4rk <b4r4n0v@gmail.com>
2026-04-27 16:26:47 +0200

    feat: add -n flag for line numbers

--- src/main.rs
+++ src/main.rs
@@ -21,18 +21,25 @@ pub fn search_with_line_numbers<'a>(pattern: &str, content: &'a str) -> Vec<(usi
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 3 {
+    let (show_line_numbers, pattern, filename) = if args.len() == 4 && args[1] == "-n" {
+        (true, &args[2], &args[3])
+    } else if args.len() == 3 {
+        (false, &args[1], &args[2])
+    } else {
         eprintln!("Usage: scour <pattern> <file>");
         process::exit(1);
-    }
-
-    let pattern = &args[1];
-    let filename = &args[2];
+    };
 
     let content = fs::read_to_string(filename).unwrap_or_else(|err| {
         eprintln!("Error reading file '{}': {}", filename, err);
         process::exit(1);
     });
+
+    if show_line_numbers {
+        for (n, line) in search_with_line_numbers(pattern, &content)  {
+            println!("{}: {}", n, line);
+        }
+    }
 
     for line in search(pattern, &content) {
         println!("{}", line);
@@ -42,6 +49,19 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use crate::{search, search_with_line_numbers};
+
+    #[test]
+    fn test_main_with_n_flag() {
+        let pattern = "fn";
+        let content = r#"let x = 1;
+fn main() {
+    println!("hello");
+}"#;
+
+        let results = search_with_line_numbers(pattern, content);
+
+        assert_eq!(results, vec![(2, "fn main() {")]);
+    }
 
     #[test]
     fn test_search_find_matching_lines() {
